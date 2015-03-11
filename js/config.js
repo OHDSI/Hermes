17752aab15a040991f7c0e8a1444fca6b40bbef5
Frank DeFalco <fdefalco@ohdsi.org>
2015-03-11 11:58:13 -0400

    Update config.js
    public configuration only

--- js/config.js
+++ js/config.js
@@ -1,13 +1,5 @@
 var ohdsi_services = [
 	{
-		name: 'Local',
-		url: 'http://localhost:8084/WebAPI/'
-	},
-	{
-		name: 'HIX',
-		url: 'http://hix.jnj.com:8080/WebAPI/'
-	},
-	{
 		name: 'Public',
 		url: 'http://api.ohdsi.org:2241/WebAPI/'
 	}
